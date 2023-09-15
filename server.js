d3ece5890cc9f4e8411987b75806d9c8dbd10279
Terence Nkeng <nterence36@gmail.com>
2023-09-15 19:31:57 -0400

    Greetings express application created

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
+// Greetings
 app.get('/greetings/:name', (req, res) => {
     res.send('<h1>Hello, '+req.params.name+ '! It\'s so great to see you!</h1>')
 })
+
+
+
 app.listen(port, function() {
     console.log('Listening on port', port)
    })
